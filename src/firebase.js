9287843f079d20405cfbc59d7e531bae865bae25
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-01-30 00:10:47 +0530

    update database

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,6 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/database";
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBZz2Y7jQpqu7lywQme8kRs77MpF5h9ZfM",
-//   authDomain: "gitfinder-5dde8.firebaseapp.com",
-//   projectId: "gitfinder-5dde8",
-//   storageBucket: "gitfinder-5dde8.appspot.com",
-//   messagingSenderId: "846943126401",
-//   appId: "1:846943126401:web:9cf71dd8b7f365c4605e80",
-//   measurementId: "G-WW75V7S61R"
-// };
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
